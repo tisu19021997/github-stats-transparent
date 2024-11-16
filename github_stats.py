26febc1030ccd91582502a617726f671070e8190
tisu19021997 <quangphamm1902@gmail.com>
2024-11-16 08:33:37 +0700

    fix: skip None node in nodes

--- github_stats.py
+++ github_stats.py
@@ -12,14 +12,20 @@ import requests
 # Main Classes
 ###############################################################################
 
+
 class Queries(object):
     """
     Class with functions to query the GitHub GraphQL (v4) API and the REST (v3)
     API. Also includes functions to dynamically generate GraphQL queries.
     """
 
-    def __init__(self, username: str, access_token: str,
-                 session: aiohttp.ClientSession, max_connections: int = 10):
+    def __init__(
+        self,
+        username: str,
+        access_token: str,
+        session: aiohttp.ClientSession,
+        max_connections: int = 10,
+    ):
         self.username = username
         self.access_token = access_token
         self.session = session
@@ -32,22 +38,24 @@ class Queries(object):
         :param generated_query: string query to be sent to the API
         :return: decoded GraphQL JSON output
         """
-        headers = {
-            "Authorization": f"Bearer {self.access_token}",
-        }
+        headers = {"Authorization": f"Bearer {self.access_token}"}
         try:
             async with self.semaphore:
-                r = await self.session.post("https://api.github.com/graphql",
-                                            headers=headers,
-                                            json={"query": generated_query})
+                r = await self.session.post(
+                    "https://api.github.com/graphql",
+                    headers=headers,
+                    json={"query": generated_query},
+                )
             return await r.json()
         except:
             print("aiohttp failed for GraphQL query")
             # Fall back on non-async requests
             async with self.semaphore:
-                r = requests.post("https://api.github.com/graphql",
-                                  headers=headers,
-                                  json={"query": generated_query})
+                r = requests.post(
+                    "https://api.github.com/graphql",
+                    headers=headers,
+                    json={"query": generated_query},
+                )
                 return r.json()
 
     async def query_rest(self, path: str, params: Optional[Dict] = None) -> Dict:
@@ -59,18 +67,18 @@ class Queries(object):
         """
 
         for _ in range(60):
-            headers = {
-                "Authorization": f"token {self.access_token}",
-            }
+            headers = {"Authorization": f"token {self.access_token}"}
             if params is None:
                 params = dict()
             if path.startswith("/"):
                 path = path[1:]
             try:
                 async with self.semaphore:
-                    r = await self.session.get(f"https://api.github.com/{path}",
-                                               headers=headers,
-                                               params=tuple(params.items()))
+                    r = await self.session.get(
+                        f"https://api.github.com/{path}",
+                        headers=headers,
+                        params=tuple(params.items()),
+                    )
                 if r.status == 202:
                     # print(f"{path} returned 202. Retrying...")
                     print(f"A path returned 202. Retrying...")
@@ -84,9 +92,11 @@ class Queries(object):
                 print("aiohttp failed for rest query")
                 # Fall back on non-async requests
                 async with self.semaphore:
-                    r = requests.get(f"https://api.github.com/{path}",
-                                     headers=headers,
-                                     params=tuple(params.items()))
+                    r = requests.get(
+                        f"https://api.github.com/{path}",
+                        headers=headers,
+                        params=tuple(params.items()),
+                    )
                     if r.status_code == 202:
                         print(f"A path returned 202. Retrying...")
                         await asyncio.sleep(2)
@@ -98,8 +108,9 @@ class Queries(object):
         return dict()
 
     @staticmethod
-    def repos_overview(contrib_cursor: Optional[str] = None,
-                       owned_cursor: Optional[str] = None) -> str:
+    def repos_overview(
+        contrib_cursor: Optional[str] = None, owned_cursor: Optional[str] = None
+    ) -> str:
         """
         :return: GraphQL query with overview of user repositories
         """
@@ -229,11 +240,16 @@ class Stats(object):
     """
     Retrieve and store statistics about GitHub usage.
     """
-    def __init__(self, username: str, access_token: str,
-                 session: aiohttp.ClientSession,
-                 exclude_repos: Optional[Set] = None,
-                 exclude_langs: Optional[Set] = None,
-                 consider_forked_repos: bool = False):
+
+    def __init__(
+        self,
+        username: str,
+        access_token: str,
+        session: aiohttp.ClientSession,
+        exclude_repos: Optional[Set] = None,
+        exclude_langs: Optional[Set] = None,
+        consider_forked_repos: bool = False,
+    ):
         self.username = username
         self._exclude_repos = set() if exclude_repos is None else exclude_repos
         self._exclude_langs = set() if exclude_langs is None else exclude_langs
@@ -247,7 +263,7 @@ class Stats(object):
         self._languages = None
         self._repos = None
         self._lines_changed = None
-        self._views = None        
+        self._views = None
 
     async def to_str(self) -> str:
         """
@@ -279,34 +295,33 @@ Languages:
         self._languages = dict()
         self._repos = set()
         self._ignored_repos = set()
-        
+
         next_owned = None
         next_contrib = None
         while True:
             raw_results = await self.queries.query(
-                Queries.repos_overview(owned_cursor=next_owned,
-                                       contrib_cursor=next_contrib)
+                Queries.repos_overview(
+                    owned_cursor=next_owned, contrib_cursor=next_contrib
+                )
             )
             raw_results = raw_results if raw_results is not None else {}
 
-            self._name = (raw_results
-                          .get("data", {})
-                          .get("viewer", {})
-                          .get("name", None))
+            self._name = raw_results.get("data", {}).get("viewer", {}).get("name", None)
             if self._name is None:
-                self._name = (raw_results
-                              .get("data", {})
-                              .get("viewer", {})
-                              .get("login", "No Name"))
+                self._name = (
+                    raw_results.get("data", {})
+                    .get("viewer", {})
+                    .get("login", "No Name")
+                )
 
-            contrib_repos = (raw_results
-                             .get("data", {})
-                             .get("viewer", {})
-                             .get("repositoriesContributedTo", {}))
-            owned_repos = (raw_results
-                           .get("data", {})
-                           .get("viewer", {})
-                           .get("repositories", {}))
+            contrib_repos = (
+                raw_results.get("data", {})
+                .get("viewer", {})
+                .get("repositoriesContributedTo", {})
+            )
+            owned_repos = (
+                raw_results.get("data", {}).get("viewer", {}).get("repositories", {})
+            )
             print("Contributed repos", contrib_repos)
             print("Owned repos", owned_repos)
             repos = owned_repos.get("nodes", [])
@@ -314,12 +329,16 @@ Languages:
                 repos += contrib_repos.get("nodes", [])
             else:
                 for repo in contrib_repos.get("nodes", []):
+                    if not repo:
+                        continue
                     name = repo.get("nameWithOwner")
                     if name in self._ignored_repos or name in self._exclude_repos:
                         continue
                     self._ignored_repos.add(name)
 
             for repo in repos:
+                if not repo:
+                    continue
                 name = repo.get("nameWithOwner")
                 if name in self._repos or name in self._exclude_repos:
                     continue
@@ -330,7 +349,8 @@ Languages:
                 for lang in repo.get("languages", {}).get("edges", []):
                     name = lang.get("node", {}).get("name", "Other")
                     languages = await self.languages
-                    if name in self._exclude_langs: continue
+                    if name in self._exclude_langs:
+                        continue
                     if name in languages:
                         languages[name]["size"] += lang.get("size", 0)
                         languages[name]["occurrences"] += 1
@@ -338,17 +358,18 @@ Languages:
                         languages[name] = {
                             "size": lang.get("size", 0),
                             "occurrences": 1,
-                            "color": lang.get("node", {}).get("color")
+                            "color": lang.get("node", {}).get("color"),
                         }
 
-            if owned_repos.get("pageInfo", {}).get("hasNextPage", False) or \
-                    contrib_repos.get("pageInfo", {}).get("hasNextPage", False):
-                next_owned = (owned_repos
-                              .get("pageInfo", {})
-                              .get("endCursor", next_owned))
-                next_contrib = (contrib_repos
-                                .get("pageInfo", {})
-                                .get("endCursor", next_contrib))
+            if owned_repos.get("pageInfo", {}).get(
+                "hasNextPage", False
+            ) or contrib_repos.get("pageInfo", {}).get("hasNextPage", False):
+                next_owned = owned_repos.get("pageInfo", {}).get(
+                    "endCursor", next_owned
+                )
+                next_contrib = contrib_repos.get("pageInfo", {}).get(
+                    "endCursor", next_contrib
+                )
             else:
                 break
 
@@ -366,7 +387,7 @@ Languages:
         if self._name is not None:
             return self._name
         await self.get_stats()
-        assert(self._name is not None)
+        assert self._name is not None
         return self._name
 
     @property
@@ -377,7 +398,7 @@ Languages:
         if self._stargazers is not None:
             return self._stargazers
         await self.get_stats()
-        assert(self._stargazers is not None)
+        assert self._stargazers is not None
         return self._stargazers
 
     @property
@@ -388,7 +409,7 @@ Languages:
         if self._forks is not None:
             return self._forks
         await self.get_stats()
-        assert(self._forks is not None)
+        assert self._forks is not None
         return self._forks
 
     @property
@@ -399,7 +420,7 @@ Languages:
         if self._languages is not None:
             return self._languages
         await self.get_stats()
-        assert(self._languages is not None)
+        assert self._languages is not None
         return self._languages
 
     @property
@@ -409,7 +430,7 @@ Languages:
         """
         if self._languages is None:
             await self.get_stats()
-            assert(self._languages is not None)
+            assert self._languages is not None
 
         return {k: v.get("prop", 0) for (k, v) in self._languages.items()}
 
@@ -421,9 +442,9 @@ Languages:
         if self._repos is not None:
             return self._repos
         await self.get_stats()
-        assert(self._repos is not None)
+        assert self._repos is not None
         return self._repos
-    
+
     @property
     async def all_repos(self) -> List[str]:
         """
@@ -433,8 +454,8 @@ Languages:
         if self._repos is not None and self._ignored_repos is not None:
             return self._repos | self._ignored_repos
         await self.get_stats()
-        assert(self._repos is not None)
-        assert(self._ignored_repos is not None)
+        assert self._repos is not None
+        assert self._ignored_repos is not None
         return self._repos | self._ignored_repos
 
     @property
@@ -446,18 +467,23 @@ Languages:
             return self._total_contributions
 
         self._total_contributions = 0
-        years = (await self.queries.query(Queries.contrib_years())) \
-            .get("data", {}) \
-            .get("viewer", {}) \
-            .get("contributionsCollection", {}) \
+        years = (
+            (await self.queries.query(Queries.contrib_years()))
+            .get("data", {})
+            .get("viewer", {})
+            .get("contributionsCollection", {})
             .get("contributionYears", [])
-        by_year = (await self.queries.query(Queries.all_contribs(years))) \
-            .get("data", {}) \
-            .get("viewer", {}).values()
+        )
+        by_year = (
+            (await self.queries.query(Queries.all_contribs(years)))
+            .get("data", {})
+            .get("viewer", {})
+            .values()
+        )
         for year in by_year:
-            self._total_contributions += year \
-                .get("contributionCalendar", {}) \
-                .get("totalContributions", 0)
+            self._total_contributions += year.get("contributionCalendar", {}).get(
+                "totalContributions", 0
+            )
         return self._total_contributions
 
     @property
@@ -473,8 +499,9 @@ Languages:
             r = await self.queries.query_rest(f"/repos/{repo}/stats/contributors")
             for author_obj in r:
                 # Handle malformed response from the API by skipping this repo
-                if (not isinstance(author_obj, dict)
-                        or not isinstance(author_obj.get("author", {}), dict)):
+                if not isinstance(author_obj, dict) or not isinstance(
+                    author_obj.get("author", {}), dict
+                ):
                     continue
                 author = author_obj.get("author", {}).get("login", "")
                 if author != self.username:
@@ -509,6 +536,7 @@ Languages:
 ###############################################################################
 # Main Function
 ###############################################################################
+
 
 async def main() -> None:
     """
